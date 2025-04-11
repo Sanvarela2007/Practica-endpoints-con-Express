2d691c19a7a5a54fc64f46766a322f79ad99f3ca
SANTIAGO VARELA <48245881@ort>
2025-04-11 10:15:32 -0300

    Final

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) =>  {
+    res.send('Bienvenido a mi servidor');
+})
+
+app.get('/saludo', (req, res) => {
+    res.send('¡Hola, mundo!')
+})
+
+app.get('/numero', (req, res) => {
+    res.send((42).toString())
+})
+
+app.get('/usuario', (req, res) => {
+    res.send({ "nombre": "Ana", "edad": 25})
+})
+
+app.get('/productos', (req, res) => {
+    res.send([ "Mouse", "Teclado", "Monitor" ])
+})
+ 
+app.get('/materias', (req, res) => {
+    res.send([ { "nombre": "Matemática" }, { "nombre": "Lengua" } ] )
+})
+
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`)
+})
